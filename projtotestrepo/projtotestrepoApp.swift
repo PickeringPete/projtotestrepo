4e5a95fd646d73cd44f9ec59718c3b635053566b
Peter Wright <peterlwright@mac.com>
2021-06-09 15:56:20 -0400

    Initial Commit

---
+++ projtotestrepo/projtotestrepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  projtotestrepoApp.swift
+//  projtotestrepo
+//
+//  Created by Peter Wright on 2021-06-09.
+//
+
+import SwiftUI
+
+@main
+struct projtotestrepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
